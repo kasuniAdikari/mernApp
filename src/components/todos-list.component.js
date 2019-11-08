2b946708aeebe1ff456a01ac13734295c99ee27a
kasuniAdikari <kasuchiranthi@gmail.com>
2019-11-08 22:35:53 +0530

    second commit

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Todolist extends Component{
+
+    render(){
+        return(
+            <div>
+                <p>welcome to TODO list...</p>
+            </div>
+        )
+    }
+}
